ebb037310f0e20d2facc1045f64107f45859e3a7
tatsuya <t2h5k0@gmail.com>
2021-09-08 19:16:34 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(
+    version = crate_version!(),
+    author = crate_authors!(),
+    about = crate_description!(),
+    setting = AppSettings::ColoredHelp,
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    println!("value of verbose: {}", opts.verbose);
 }
